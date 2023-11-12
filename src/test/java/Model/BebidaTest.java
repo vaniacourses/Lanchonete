21369bf34b2337366c70fd0926cb4415ea0255c6
Evandro Mauricio <evandromauricio@id.uff.br>
2023-11-12 03:27:47 -0300

    BebidaTest 01

--- src/test/java/Model/BebidaTest.java
+++ src/test/java/Model/BebidaTest.java
@@ -1,5 +1,26 @@
 package Model;
 
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+
+
 public class BebidaTest {
 
+    @Test
+    public void testGetSetId_bebida() {
+        Bebida bebida = new Bebida();
+        bebida.setId_bebida(1);
+        assertEquals(1, bebida.getId_bebida());
+    }
+
+    @Test
+    public void testGetSetNome() {
+        Bebida bebida = new Bebida();
+        bebida.setNome("Coca Cola");
+        assertEquals("Coca Cola", bebida.getNome());
+    }
+
+
 }
+
