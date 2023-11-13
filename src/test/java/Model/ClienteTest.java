4621094af211f7fe246598bfa5897ef4bb89a144
Evandro Mauricio <evandromauricio@id.uff.br>
2023-11-13 18:10:41 -0300

    ClienteTest

--- src/test/java/Model/ClienteTest.java
+++ src/test/java/Model/ClienteTest.java
@@ -1,5 +1,81 @@
 package Model;
 
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 public class ClienteTest {
+
+    @Test
+    public void testGetSetIdCliente() {
+        Cliente cliente = new Cliente();
+        cliente.setIdCliente(1);
+        assertEquals(1, cliente.getIdCliente());
+    }
+
+    @Test
+    public void testGetSetNome() {
+        Cliente cliente = new Cliente();
+        cliente.setNome("Evandro");
+        assertEquals("Evandro", cliente.getNome());
+    }
+
+    @Test
+    public void testGetSetSobrenome() {
+        Cliente cliente = new Cliente();
+        cliente.setSobrenome("Mauricio");
+        assertEquals("Mauricio", cliente.getSobrenome());
+    }
+
+    @Test
+    public void testGetSetTelefone() {
+        Cliente cliente = new Cliente();
+        cliente.setTelefone("21991328474");
+        assertEquals("21991328474", cliente.getTelefone());
+    }
+
+    @Test
+    public void testGetSetUsuario() {
+        Cliente cliente = new Cliente();
+        cliente.setUsuario("EvandroMauricio");
+        assertEquals("EvandroMauricio", cliente.getUsuario());
+    }
+
+    @Test
+    public void testGetSetSenha() {
+        Cliente cliente = new Cliente();
+        cliente.setSenha("minhasenha");
+        assertEquals("minhasenha", cliente.getSenha());
+    }
+
+    @Test
+    public void testGetSetFgAtivo() {
+        Cliente cliente = new Cliente();
+        cliente.setFgAtivo(1);
+        assertEquals(1, cliente.getFgAtivo());
+    }
+
+    @Test
+    public void testGetSetEndereco() {
+        Cliente cliente = new Cliente();
+        Endereco endereco = new Endereco();
+        cliente.setEndereco(endereco);
+        assertEquals(endereco, cliente.getEndereco());
+    }
+
+    @Test
+    public void testResumo() {
+        Cliente cliente = new Cliente();
+        cliente.setNome("Evandro");
+        cliente.setSobrenome("Mauricio");
+        cliente.setTelefone("21991328474");
+        cliente.setUsuario("EvandroMauricio");
+        cliente.setSenha("minhasenha");
+
+        String expectedResumo = "Nome: Evandro Mauricio <br>Telefone: 21991328474 <br>Usuario: EvandroMauricio <br>Senha: minhasenha";
+
+        assertEquals(expectedResumo, cliente.resumo());
+    }
+
+    // Add more test methods for other functionalities
 
 }
