5b88a1ca54a83698f0012f87564b65fd4ccfda25
Evandro Mauricio <evandromauricio@id.uff.br>
2023-11-21 06:53:20 -0300

    mockito_1

--- src/main/java/Model/Bebida.java
+++ src/main/java/Model/Bebida.java
@@ -11,66 +11,66 @@ public class Bebida {
     private String tipo;
     private int fg_ativo;
 
+    // ID BEBIDA
     public int getId_bebida() {
         return id_bebida;
     }
-
     public void setId_bebida(int id_bebida) {
         this.id_bebida = id_bebida;
     }
 
+    // NOME
     public String getNome() {
         return nome;
     }
-
     public void setNome(String nome) {
         this.nome = nome;
     }
 
+    // DESCRICAO
     public String getDescricao() {
         return descricao;
     }
-
     public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
 
+    // QUANTIDADE
     public int getQuantidade() {
         return quantidade;
     }
-
     public void setQuantidade(int quantidade) {
         this.quantidade = quantidade;
     }
 
+    // VALOR COMPRA
     public Double getValor_compra() {
         return valor_compra;
     }
-
     public void setValor_compra(Double valor_compra) {
         this.valor_compra = valor_compra;
     }
 
+    // VALOR VENDA
     public Double getValor_venda() {
         return valor_venda;
     }
-
     public void setValor_venda(Double valor_venda) {
         this.valor_venda = valor_venda;
     }
 
+    // TIPO
     public String getTipo() {
         return tipo;
     }
-
     public void setTipo(String tipo) {
         this.tipo = tipo;
     }
 
+    // FG_ATIVO
     public int getFg_ativo() {
         return fg_ativo;
     }
-
     public void setFg_ativo(int fg_ativo) {
         this.fg_ativo = fg_ativo;
     }
--- src/test/java/Model/BebidaTest.java
+++ src/test/java/Model/BebidaTest.java
@@ -2,132 +2,170 @@ package Model;
 
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+import static org.mockito.Mockito.*;
 
 
 public class BebidaTest {
-
+	
+	// ---------- TESTE ID BEBIDA ----------------
 	@Test
-    public void testa_GetId_bebida() {
+    public void testGetId_bebida() {
         Bebida bebida = new Bebida();
         bebida.setId_bebida(1);
         assertEquals(1, bebida.getId_bebida());
     } // Este teste verifica se o método getId_bebida() retorna o valor correto quando o atributo id_bebida da classe Bebida é definido.
-	 
-	
 	@Test
-    public void testa_SetId_bebida() {
+    public void testGetIdBebidaComMockito() {
+        Bebida bebidaMock = mock(Bebida.class);
+        int idBebidaEsperado = 88;
+        when(bebidaMock.getId_bebida()).thenReturn(idBebidaEsperado);
+        int idBebidaObtido = bebidaMock.getId_bebida();
+        assertEquals(idBebidaEsperado, idBebidaObtido);
+    }
+
+	@Test
+    public void testSetId_bebida() {
         Bebida bebida = new Bebida();
         bebida.setId_bebida(1);
         assertEquals(1, bebida.getId_bebida());
     } // Este teste verifica se o método setId_bebida() altera o valor do atributo id_bebida da classe Bebida corretamente.
-    
-    
 	@Test
-    public void testa_GetNome() {
+	public void testSetIdBebidaComMockito() {
+        Bebida bebidaMock = mock(Bebida.class);
+        int idBebidaEsperado = 51;
+        bebidaMock.setId_bebida(idBebidaEsperado);
+        verify(bebidaMock).setId_bebida(idBebidaEsperado);
+    }
+    
+	// ---------- TESTE NOME ---------------------
+	@Test
+    public void testGetNome() {
         Bebida refrigerante = new Bebida();
         refrigerante.setNome("Guarana");
         assertEquals("Guarana", refrigerante.getNome());
     } // Este teste verifica se o método getNome() retorna o valor correto quando o atributo nome da classe Bebida é definido.
-	
-	
 	@Test
-    public void testa_SetNome() {
+    public void testGetNomeComMockito() {
+        Bebida bebidaMock = mock(Bebida.class);
+        String nomeEsperado = "Kuat";
+        when(bebidaMock.getNome()).thenReturn(nomeEsperado);
+        String nomeObtido = bebidaMock.getNome();
+        assertEquals(nomeEsperado, nomeObtido);
+    }
+	
+
+	@Test
+    public void testSetNome() {
         Bebida refrigerante = new Bebida();
         refrigerante.setNome("Coca-Cola");
         assertEquals("Coca-Cola", refrigerante.getNome());
     } // Este teste verifica se o método setNome() altera o valor do atributo nome da classe Bebida corretamente.
-	
+	@Test
+    public void testSetNomeComMockito() {
+        Bebida bebidaMock = mock(Bebida.class);
+        String nomeEsperado = "Fanta";
+        bebidaMock.setNome(nomeEsperado);
+        verify(bebidaMock).setNome(nomeEsperado);
+    }
+		
     
+	// ---------- TESTE DESCRICAO ----------------
     @Test
-    public void testa_SetDescricao() {
+    public void testSetDescricao() {
     	Bebida Dolly = new Bebida();
     	Dolly.setDescricao("O sabor Brasileiro");
-    }
-
-    
+    }    
     @Test
-    public void testa_GetDescricao() {
+    public void testGetDescricao() {
     	Bebida Dolly = new Bebida();
     	Dolly.setDescricao("O sabor Brasileiro");
     	assertEquals("O sabor Brasileiro", Dolly.getDescricao() );
     }
  
-    
+    // ---------- TEST QUANTIDADE -------------------
     @Test
-    public void testa_GetQuantidade() {
+    public void testGetQuantidade() {
+        Bebida bebida = new Bebida();
+        bebida.setQuantidade(10);
+        assertEquals(10, bebida.getQuantidade());
+    }
+    @Test
+    public void testSetQuantidade() {
         Bebida bebida = new Bebida();
         bebida.setQuantidade(10);
         assertEquals(10, bebida.getQuantidade());
     }
 
+    // ---------- TESTE VALOR COMPRA  ----------------
     @Test
-    public void testa_SetQuantidade() {
+    public void testSetValorCompra() {
         Bebida bebida = new Bebida();
-        bebida.setQuantidade(10);
-        assertEquals(10, bebida.getQuantidade());
-    }
+        bebida.setValor_compra(5.0);
 
-    // TESTE INCOMPLETO
-    @Test
-    public void testa_GetValor_compra() {
-        Bebida bebida = new Bebida();
-        bebida.setValor_compra(10.00);    
+        assertEquals(5.0, bebida.getValor_compra(), 0.01);
     }
-
-    // TESTE INCOMPLETO
     @Test
-    public void testa_SetValor_compra() {
+    public void testSetValor_compra() {
         Bebida bebida = new Bebida();
         bebida.setValor_compra(10.00);
     }
 
-    // TESTE INCOMPLETO
+    // ---------- TESTE VALOR VENDA	----------------
     @Test
-    public void testa_GetValor_venda() {
+    public void testGetValorVenda() {
+        Bebida bebida = new Bebida();								// Criação de uma instância de Bebida
+        double valorVendaEsperado = 15.0;							// Definição de um valor de venda para a bebida
+        bebida.setValor_venda(valorVendaEsperado);
+        double valorVendaObtido = bebida.getValor_venda();			// Chamada do método que queremos testar
+        assertEquals(valorVendaEsperado, valorVendaObtido, 0.01); 	// Verificação do resultado; Usando uma margem de erro para comparação de valores de ponto flutuante
+    }
+    @Test
+    public void testGetValorVenda_Mockito() {
+        Bebida bebidaMock = mock(Bebida.class);
+        double valorVendaEsperado = 15.0;
+        when(bebidaMock.getValor_venda()).thenReturn(valorVendaEsperado);
+        double valorVendaObtido = bebidaMock.getValor_venda();
+        assertEquals(valorVendaEsperado, valorVendaObtido, 0.01);
+    }
+    @Test
+    public void testSetValor_venda() {
         Bebida bebida = new Bebida();
         bebida.setValor_venda(15.00);
     }
-
-    // TESTE INCOMPLETO
     @Test
-    public void testa_SetValor_venda() {
-        Bebida bebida = new Bebida();
-        bebida.setValor_venda(15.00);
+    public void testSetValorVenda_Mockito() {
+        Bebida bebidaMock = mock(Bebida.class);
+        double valorVendaEsperado = 15.0;
+        bebidaMock.setValor_venda(valorVendaEsperado);
+        verify(bebidaMock).setValor_venda(valorVendaEsperado);
     }
  
-    
+    // ---------- TESTE TIPO	----------------
     @Test
-    public void testa_GetTipo() {
+    public void testGetTipo() {
         Bebida suco = new Bebida();
         suco.setTipo("Suco Natural");
         assertEquals("Suco Natural", suco.getTipo());
     }
-
     @Test
-    public void testa_SetTipo() {
+    public void testSetTipo() {
         Bebida bebida = new Bebida();
         bebida.setTipo("Cerveja");
         assertEquals("Cerveja", bebida.getTipo());
     }
      
-    
+    // ---------- TESTE FG_ATIVO	----------------
     @Test
-    public void testa_GetFg_ativo() {
+    public void testGetFg_ativo() {
         Bebida bebida = new Bebida();
         bebida.setFg_ativo(1);
         assertEquals(1, bebida.getFg_ativo());
     } // Este teste verifica se o método getFg_ativo() retorna o valor correto quando o atributo fg_ativo da classe Bebida é definido.
     
-    
     @Test
-    public void testa_SetFg_ativo() {
+    public void testSetFg_ativo() {
         Bebida bebida = new Bebida();
         bebida.setFg_ativo(1);
         assertEquals(1, bebida.getFg_ativo());
     } // Este teste verifica se o método setFg_ativo() altera o valor do atributo fg_ativo da classe Bebida corretamente.
-    
-    
-    
-
 }
-
